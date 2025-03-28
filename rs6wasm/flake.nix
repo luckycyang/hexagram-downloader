61377c230df3ccf9bf91f8c2979a949a8bab2c3c
luckycyang <kfove.liang@protonmail.com>
2025-03-28 23:30:32 +0800

    add flake.nix

---
+++ rs6wasm/flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustpkg = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
+          toolchain.default.override {
+            extensions = ["rust-src" "rustfmt" "clippy" "rust-analyzer"]; # rust-src for rust-analyzer
+            targets = ["x86_64-unknown-linux-gnu" "thumbv7m-none-eabi"];
+          });
+      in
+        with pkgs; {
+          devShells = {
+            default = mkShell {
+              buildInputs = [
+                openssl
+                pkg-config
+                eza
+                fd
+                rustpkg
+              ];
+              shellHook = ''
+                alias ls=eza
+                alias find=fd
+              '';
+            };
+          };
+        }
+    );
+}
